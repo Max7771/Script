5e31846739984de6a4edc24a7b3d2117070ea880
Max7771 <Max7771@users.noreply.github.com>
2024-06-10 09:27:25 +0300

    Update OMRR.user.js

--- OMRR.user.js
+++ OMRR.user.js
@@ -20,7 +20,7 @@
     }
 
     // Retrieve the script from GitHub raw file
-    fetch('https://raw.githubusercontent.com/Max7771/Script/main/v052024.3')
+    fetch('https://raw.githubusercontent.com/Max7771/Script/main/v062024.4')
         .then(response => response.text())
         .then(script => {
             injectScript(script);
