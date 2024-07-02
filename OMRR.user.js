5fab9598723d7f5c79a104acbf693d2fd68d23f4
Max7771 <Max7771@users.noreply.github.com>
2024-07-02 17:25:59 +0300

    Update OMRR.user.js

--- OMRR.user.js
+++ OMRR.user.js
@@ -2,7 +2,7 @@
 // @name         OMRR
 // @namespace    Test IO – Education Team – Onboarding
 // @author       Charlie
-// @version      2.0
+// @version      5.0
 // @description  Implementing the expanded text abbreviations into buttons functionality to the official specific rejection reasons for Test IO Onboarding tickets into their complete forms and banners to highlight relevant tickets based on the tester’s location (yellow and red).
 // @icon         https://test.io/content/dam/test-io/test_io_logo1.svg
 // @match        https://onboardingmanager.test.io/managers/*
@@ -20,7 +20,7 @@
     }
 
     // Retrieve the script from GitHub raw file
-    fetch('https://raw.githubusercontent.com/Max7771/Script/main/v062024.4')
+    fetch('https://raw.githubusercontent.com/CharlieMrFox/TestIO_CE_OManagersRRs/main/v072024.5')
         .then(response => response.text())
         .then(script => {
             injectScript(script);
